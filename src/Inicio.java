2e10ecc934853b5f244725e57f036158c749bee6
Jaqueline <csi23-jromres@colegioaltair.net>
2023-12-19 12:51:29 +0100

    Ejercicio4BuclesJava

---
+++ src/Inicio.java
@@ -0,0 +1,34 @@
+
+
+		package Ejercicio4Bucles;
+
+		import java.util.Scanner;
+
+		public class Inicio {
+
+		   public static void main(String[] args) {
+		       
+		       Scanner sc = new Scanner(System.in);
+		       long aux = 0;
+		       long aux1 = 1;
+		       long suma;
+		       
+		       
+		       System.out.println("Cuantas posiciones de la serie de Fibo quieres: ");
+		       int limiteSecuencia = sc.nextInt();
+		       
+		       // 0 1 1 2
+		       System.out.print(1 + " ");
+		       for(int i=1; i<= limiteSecuencia; i++) {
+		           
+		          
+		         
+
+		           
+		       }
+		   }
+
+		}
+	}
+
+}
